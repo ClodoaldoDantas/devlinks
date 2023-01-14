d996cc32b8871cbfe05477d231bbc2e6e0b9e4ba
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2023-01-14 10:32:29 -0300

    refactor(useFetch): melhorando a logica do hook

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,24 +1,28 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { http } from '../services/http'
 
 export function useFetch<T = unknown>(url: string) {
   const [data, setData] = useState<T | null>(null)
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState<any>(null)
 
-  useEffect(() => {
-    http
-      .get(url)
-      .then((response) => {
-        setData(response.data)
-      })
-      .catch((err) => {
-        setError(err)
-      })
-      .finally(() => {
-        setIsLoading(false)
-      })
+  const doFetch = useCallback(async () => {
+    try {
+      setIsLoading(true)
+      setError(null)
+
+      const response = await http.get(url)
+      setData(response.data)
+    } catch (err) {
+      setError(err)
+    } finally {
+      setIsLoading(false)
+    }
   }, [url])
 
-  return { data, isLoading, isError: !!error, error }
+  useEffect(() => {
+    doFetch()
+  }, [doFetch])
+
+  return { data, isLoading, isError: !!error, error, doFetch }
 }
